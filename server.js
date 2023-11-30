75509b8d636cec4f03df9f22f290242244d52eac
Marcio Ferreira <profhaeliomarcio@gmail.com>
2023-11-29 21:29:41 -0300

    Ajustes 2

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors';
 import RotasPublicas from './routes/RotasPublicas.js';
 import RotasPrivadas from './routes/RotasPrivadas.js';
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 
 // VALIDACAO DE DADOS
 // UPLOAD DE IMAGEM - 
